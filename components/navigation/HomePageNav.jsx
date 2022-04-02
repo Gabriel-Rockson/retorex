8139f829707be1b339b66358de7b6f26eb6fc4be
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-02 20:40:27 +0000

    Add homepage navigation component

---
+++ components/navigation/HomePageNav.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Flex,
+  Spacer,
+  Box,
+  Text,
+  Button,
+  Heading,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { BsCart3 } from "react-icons/bs";
+import { BiShowAlt } from "react-icons/bi";
+import { ImBin2 } from "react-icons/im";
+import CartButton from "./CartButton";
+
+function HomePageNav() {
+  return (
+    <>
+      <Flex bg="black" px={[2, 10]} py={2} alignItems="center">
+        <Box>
+          <Heading size="md" color="white">
+            Retorex
+          </Heading>
+        </Box>
+        <Spacer />
+        <Flex>
+          <Button mr={[2, 4]} colorScheme="blue">
+            Start Selling
+          </Button>
+          <Menu>
+            <MenuButton>
+              <Button colorScheme="whiteAlpha" variant="ghost">
+                <Text mr="2">Cart</Text>
+                <Icon as={BsCart3} fontSize={"2xl"} color="white" />
+                <sup style={{ fontSize: "16px" }}>3</sup>
+              </Button>
+            </MenuButton>
+            <MenuList>
+              <MenuItem icon={<BiShowAlt />}>Show Items</MenuItem>
+              <MenuItem icon={<ImBin2 />}>Clear Items</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
+
+export default HomePageNav;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import HomePageNav from "../components/navigation/HomePageNav";
 
 const HomePage = () => {
-  return <h1>This is the homepage</h1>;
+  return (
+    <div>
+      <HomePageNav />
+      <p>Main Area</p>
+      <ul>
+        <li>List the currently added items</li>
+        <li>People should be able to add them to their carts by clicking on buy</li>
+      </ul>
+      <p>Footer</p>
+    </div>
+  )
 };
 
 export default HomePage;
