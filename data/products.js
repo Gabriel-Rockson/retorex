f7c91c1d2e94316230093ba87051289ec122441e
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-05 23:37:38 +0000

    Add more data

--- data/products.js
+++ data/products.js
@@ -4,7 +4,7 @@ export const products = [
     image: "https://m.media-amazon.com/images/I/91CktmxU6dL._AC_UL320_.jpg",
     title: "Rock Paper Scissors: A Novel",
     seller: "Alice Feeney",
-    price: "$3.99",
+    price: "3.99",
     category: "Ebook",
   },
   {
@@ -12,7 +12,7 @@ export const products = [
     image: "https://m.media-amazon.com/images/I/91uyXpTwqkL._AC_UL320_.jpg",
     title: "The Last Green Valley",
     seller: "Mark Sullivan",
-    price: "$44.23",
+    price: "44.23",
     category: "Web Design Template",
   },
   {
@@ -20,7 +20,7 @@ export const products = [
     image: "https://m.media-amazon.com/images/I/81T8YStTq2L._AC_UL320_.jpg",
     title: "All About Me",
     seller: "Mel Brooks",
-    price: "$2.99",
+    price: "2.99",
     category: "Biography Sheet",
   },
   {
@@ -29,7 +29,7 @@ export const products = [
     title:
       "This is how they tell me the world Ends: The cyberweapons arms race.",
     seller: "Nicole Perlroth",
-    price: "$2.99",
+    price: "2.99",
     category: "Research Paper",
   },
   {
@@ -37,7 +37,41 @@ export const products = [
     image: "https://m.media-amazon.com/images/I/81jj6t5GkIL._AC_UL320_.jpg",
     title: "Hell of a Book",
     seller: "Jason Matt",
-    price: "$1.99",
+    price: "1.99",
+    category: "Ebook",
+  },
+  {
+    id: 6,
+    image: "https://m.media-amazon.com/images/I/81RxHT4G5hL._AC_UL320_.jpg",
+    title:
+      "This is how they tell me the world Ends: The cyberweapons arms race.",
+    seller: "Nicole Perlroth",
+    price: "2.99",
+    category: "Research Paper",
+  },
+  {
+    id: 7,
+    image: "https://m.media-amazon.com/images/I/81jj6t5GkIL._AC_UL320_.jpg",
+    title: "Hell of a Book",
+    seller: "Jason Matt",
+    price: "1.99",
+    category: "Ebook",
+  },
+  {
+    id: 8,
+    image: "https://m.media-amazon.com/images/I/81RxHT4G5hL._AC_UL320_.jpg",
+    title:
+      "This is how they tell me the world Ends: The cyberweapons arms race.",
+    seller: "Nicole Perlroth",
+    price: "2.99",
+    category: "Research Paper",
+  },
+  {
+    id: 9,
+    image: "https://m.media-amazon.com/images/I/81jj6t5GkIL._AC_UL320_.jpg",
+    title: "Hell of a Book",
+    seller: "Jason Matt",
+    price: "1.99",
     category: "Ebook",
   },
 ];
