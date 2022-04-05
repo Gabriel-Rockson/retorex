3b93a28787c69bb338c54d195d86d4b84b52f1b9
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-05 23:41:33 +0000

    Show the data in the basket on the cart page

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,18 +1,72 @@
-import { Box } from "@chakra-ui/react";
-import React, {useContext} from "react";
+import {
+  Box,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  TableCaption,
+} from "@chakra-ui/react";
+import React, { useContext } from "react";
 
 import { CartContext } from "../../context/cartContext";
+import ProductList from "../../components/products/ProductList";
+import ProductItem from "../../components/products/ProductItem";
 
 function CartPage() {
-  const {cartItems, setCartItems} = useContext(CartContext)
+  const { cartItems, setCartItems } = useContext(CartContext);
 
   return (
     <Box>
-      {cartItems.map(item => (
-        <Box key={item.id}>
-          {item.title}
+      <Box mb={5} width={["100%", null, null, "60%"]}>
+        <Box>
+          <TableContainer>
+            <Table size={"md"} variant={"striped"} colorScheme={"linkedin"}>
+              <TableCaption>Cart Summary</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Item</Th>
+                  <Th>Quantity</Th>
+                  <Th>Price</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {cartItems?.map((item) => (
+                  <Tr>
+                    <Td>{item.title}</Td>
+                    <Td isNumeric>1</Td>
+                    <Td isNumeric>{item.price}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+              <Thead>
+                <Tr>
+                  <Th></Th>
+                  <Th>Amount Payable</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>All Items</Td>
+                  <Td>
+                    {cartItems
+                      ?.map((item) => Number(item.price))
+                      .reduce((a, b) => a + b, 0)}
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
         </Box>
-      ))}
+      </Box>
+
+      <ProductList products={cartItems} />
+
+      <Box>Checkout</Box>
     </Box>
   );
 }
